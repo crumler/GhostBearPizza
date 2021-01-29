46f03a6cab77bb219e8d995d037a5c2f0a5c9691
Chris Rumler <cgrumler@gmail.com>
2021-01-29 16:57:21 -0500

    begun script and changed order layout

---
+++ script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+let products = [
+    {
+        name: "Spicy Pepperoni Pizza",
+        tag: "spicypepperoni",
+        price: 20,
+        inCart: 0
+    },
+    {
+        name: "Zesty Chorizo Pizza",
+        tag: "zestychorizo",
+        price: 20,
+        inCart: 0
+    },
+    {
+        name: "Savory Pineapple Pizza",
+        tag: "savorypineapple",
+        price: 20,
+        inCart: 0
+    },
+    {
+        name: "The Alpha Strike",
+        tag: "alphastrike",
+        price: 30,
+        inCart: 0
+    },
+    {
+        name: "Spicy Pepperoni Pizza",
+        tag: "spicypepperoni",
+        price: 20,
+        inCart: 0
+    },
+    {
+        name: "Breadsticks",
+        tag: "breadsticks",
+        price: 5,
+        inCart: 0
+    },
+    {
+        name: "Stuffed Breadsticks",
+        tag: "stuffedbreadsticks",
+        price: 8,
+        inCart: 0
+    },
+    {
+        name: "Garlic Cheese Bread",
+        tag: "cheesebread",
+        price: 7,
+        inCart: 0
+    },
+    {
+        name: "Chorizo Stuffed Jalapeno Poppers",
+        tag: "poppers",
+        price: 10,
+        inCart: 0
+    }
+];
+
